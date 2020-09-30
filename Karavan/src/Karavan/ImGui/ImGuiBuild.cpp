16d3bbe6d2453ff130147e84794da71296c1f08f
Ashish Mittal <ashish_mittal@apple.com>
2020-09-30 14:46:16 -0500

    Cleaned up a warning

--- Karavan/src/Karavan/ImGui/ImGuiBuild.cpp
+++ Karavan/src/Karavan/ImGui/ImGuiBuild.cpp
@@ -1,5 +1,5 @@
 #include "KVPCH.h"
 
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+//#define IMGUI_IMPL_OPENGL_LOADER_GLAD
 #include "examples/imgui_impl_opengl3.cpp"
 #include "examples/imgui_impl_glfw.cpp"
