f9e0ead6c2b0760238a326b0be765bee2dae908f
Ashish Mittal <ashish_mittal@apple.com>
2020-10-02 16:50:36 -0500

    Added second vertexarray with square

--- Karavan/src/Karavan/Application.h
+++ Karavan/src/Karavan/Application.h
@@ -30,8 +30,9 @@ namespace Karavan {
         LayerStack m_LayerStack;
         std::shared_ptr<Shader> m_Shader;
         std::shared_ptr<VertexArray> m_VertexArray;
-        std::shared_ptr<VertexBuffer> m_VertexBuffer;
-        std::shared_ptr<IndexBuffer> m_IndexBuffer;
+
+        std::shared_ptr<Shader> m_BlueShader;
+        std::shared_ptr<VertexArray> m_SquareVA;
     private:
         static Application* s_Instance;
     };
