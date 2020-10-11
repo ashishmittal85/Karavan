a49427ef5d9be6a05c888efe58cd97814a3dbb5e
Ashish Mittal <ashishmittal@Ashishs-MacBook-Pro.local>
2020-10-11 18:34:04 -0500

    Added support for transforms

--- Karavan/src/Karavan/Renderer/Renderer.h
+++ Karavan/src/Karavan/Renderer/Renderer.h
@@ -14,7 +14,7 @@ namespace Karavan {
         static void BeginScene(OrthographicCamera& camera);
         static void EndScene();
 
-        static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+        static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f));
     private:
         struct SceneData
         {
